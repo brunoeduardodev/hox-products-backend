9bef195a9e4ba4f7861c38195ef99cd7ea3181cb
brunoeduardodev <brunoeduardo.dev@gmail.com>
2021-01-25 14:34:40 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import Express from 'express'
+import cors from 'cors'
 import '@infra/mongoose/connection'
 import routes from './routes'
 
 const server = Express()
 
 server.use(Express.json())
+server.use(cors({}))
 server.use(routes)
 const port = process.env.PORT || 3333
 
